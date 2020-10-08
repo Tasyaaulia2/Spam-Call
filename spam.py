5a315e826a6b8a059ec60466c886b3a837587107
SanzXp <51875646+B4N954N2-ID@users.noreply.github.com>
2020-10-09 06:39:36 +0700

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,16 @@
+import os
+'''
+Created by Sanz
+Youtube : TUTORIAL ANDROID
+Github  : https://github.com/B4N954N2-ID
+
+Hay Bwang Jago jangan recode script gua donk asu.
+Bikin sendiri ngapa njink yahaha !
+
+'''
+os.system('python2 .send.py')
+'''
+
+# Simple kan sc bikinan gua yahaha :v
+
+'''
